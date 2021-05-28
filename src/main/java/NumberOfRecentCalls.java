5eb50a276e9f3401916636c6bf4b1eab71b06d05
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-05-28 09:40:09 +0530

    Added problem for Number of Recent Calls.

---
+++ src/main/java/NumberOfRecentCalls.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/number-of-recent-calls/
+
+/*
+The idea is to implement a class with below functionalities:
+Constructor will initialize the object.
+Method ping will accept time t. This will be called multiple times.
+Problem is at each invocation of ping, method should return how many requests
+has happened in last 3000 milliseconds. In other words, find number of times
+ping was called during t and t - 3000 (both inclusive).
+ping(1) -> total requests : {1}. There are total 1 requests in range -2999,1
+ping(100) -> total requests : {1,100}. There are total 2 requests in range
+-2900,100
+ping(3001) -> total requests : {1,100,3001}. There are total 3 requests in
+range 1,3001
+ping(3002) -> total requests : {1,100,3001,3002}. There are total 3 requests in
+range 2,3002
+
+
+
+*/
+
+public class NumberOfRecentCalls {
+    public NumberOfRecentCalls() {
+
+    }
+
+    public int ping(int t) {
+
+    }
+
+    public static void main(String[] args) {
+        NumberOfRecentCalls numberOfRecentCalls = new NumberOfRecentCalls();
+        System.out.println(numberOfRecentCalls.ping(1));
+        System.out.println(numberOfRecentCalls.ping(100));
+        System.out.println(numberOfRecentCalls.ping(3001));
+        System.out.println(numberOfRecentCalls.ping(3002));
+    }
+}
