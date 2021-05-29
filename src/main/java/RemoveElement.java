dd0256fadff7c59c7a754a6e677578ab370d96d8
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-05-29 13:52:58 +0530

    Added problem statement.

--- src/main/java/RemoveElement.java
+++ src/main/java/RemoveElement.java
@@ -1,2 +1,11 @@
+//https://leetcode.com/problems/remove-element/
+/*
+Method is given an input array containing 0 or more duplicate elements. Also,
+ a number is provided which is supposed to be removed from array.
+The method should remove all occurrences of given element from given array
+'in place' - i.e. without using any extra array, and actual input array should
+be modified. Further method should return length of new array after removing
+all occurrences of given element.
+ */
 public class RemoveElement {
 }
