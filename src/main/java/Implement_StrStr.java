ccc1b47ac0b6fda9565b4b001f30a5e3d738c8e6
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-05-30 09:20:04 +0530

    Implement StrStr problem.

---
+++ src/main/java/Implement_StrStr.java
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/implement-strstr/
+/*
+Problem is to find out if given string is substring of another string.
+If it is substring, then return first index at which substring occurs.
+Else, return -1.
+ */
+public class Implement_StrStr {
+}
